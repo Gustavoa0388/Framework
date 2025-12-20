1dbe22a15537ed83d3ee83d15ada7477792819a1
Gustavo Silva <gustavoa18@gmail.com>
2025-12-19 22:36:23 -0300

    Add GsHintLabel for helper text with proper Light/Dark theming

---
+++ GS.Core/Controls/GsHintLabel.cs
@@ -0,0 +1,45 @@
+﻿using GS.Core.UI.Theming;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GS.Core.UI.Controls
+{
+    public class GsHintLabel : Label, IThemedControl
+    {
+        public GsHintLabel()
+        {
+            AutoSize = true;
+            BackColor = Color.Transparent;
+            TextAlign = ContentAlignment.MiddleLeft;
+            UseMnemonic = false;
+
+            ApplyFont();
+        }
+
+        private void ApplyFont()
+        {
+            Font = new Font(
+                Font.FontFamily,
+                9f,
+                FontStyle.Regular,
+                GraphicsUnit.Point
+            );
+        }
+
+        public void ApplyTheme(GsTheme theme)
+        {
+            BackColor = Color.Transparent;
+
+            ForeColor = theme.IsDark
+                ? Color.FromArgb(170, 170, 170)   // cinza claro no dark
+                : Color.FromArgb(90, 90, 90);     // cinza discreto no light
+
+            Font = theme.DefaultFont != null
+                ? new Font(theme.DefaultFont.FontFamily, 9f, FontStyle.Regular)
+                : Font;
+
+            Invalidate();
+        }
+    }
+}
